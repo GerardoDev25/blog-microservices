179727c9d94cdbd5dc41b4bb3aae7b41c945a300
Gerardo Miranda <gerardodev25@gmail.com>
2025-10-22 16:26:23 -0400

    ci(infra): :bricks: create client deployment, create ingress service and wire up all

--- client/src/PostCreate.jsx
+++ client/src/PostCreate.jsx
@@ -7,7 +7,7 @@ const PostCreate = () => {
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    const resp = await axios.post('http://localhost:4000/posts', {
+    const resp = await axios.post('http://posts.com/posts/create', {
       title,
     });
     console.log(resp.data);
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    // THIS IS THE CRITICAL LINE
+    host: '0.0.0.0',
+    port: 5173, // Ensure this matches your expected port
+
+    // 2. THIS IS THE NEW CRITICAL LINE to fix the "Blocked request"
+    allowedHosts: ['posts.com'],
+  },
+});
